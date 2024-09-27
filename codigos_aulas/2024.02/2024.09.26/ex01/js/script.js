640bc4cdc891a0684ede3a669a86623e7731fd77
Futuresenaweb <futuresenaweb@gmail.com>
2024-09-27 14:54:38 -0300

    Upload aula 26/09/24

---
+++ codigos_aulas/2024.02/2024.09.26/ex01/js/script.js
@@ -0,0 +1,175 @@
+// var x = 10;
+// var y = "wilton"
+// alert(`O valor de x = ${x - 2} ${y}`);
+
+// btn = confirm("Deseja cadastrar?");
+// if (btn == true) { // btn ok pressionado
+//     alert("btn ok pressionado")
+// }
+// else { // btn cancelar pressionado
+//     alert("btn cancelar pressionado")
+// }
+
+// if (confirm("Deseja cadastrar?")) { // btn ok pressionado
+//     alert("btn ok pressionado")
+// }
+// else { // btn cancelar pressionado
+//     alert("btn cancelar pressionado")
+// }
+
+// nome = prompt("Qual o seu nome?","wilton");
+// alert(`O nome digitado foi ${nome}`)
+// alert(`${nome}`);
+// alert(nome);
+// alert("O nome digitado foi " + nome)
+
+// idade1 = parseInt(prompt("Digite a idade da primeira pessoa:"));
+// idade2 = parseInt(prompt("Digite a idade da segunda pessoa:"));
+
+// soma = idade1 + idade2;
+// // soma = parseInt(idade1) + parseInt(idade2);
+// console.log(soma);
+
+// if (soma >= 130) 
+//     alert("Uma das pessoas é idosa")
+// else alert("Ninguém é idoso")
+
+// x = 10;
+// console.log(typeof(x)); // number
+
+// x = "10";
+// console.log(typeof(x)); // string
+
+// x = true;
+// console.log(typeof(x)); // boolean
+
+// const PI = 3.14;
+// var raio = 12;
+// var area;
+
+// area = PI * raio*raio;
+
+// console.log(area);
+
+// // Declarar uma variável e atribuir a ela o nome dessa disciplina
+// var disciplina = "JavaScript Básico";
+
+// // Na variável anterior adicionar ao final o nome do Professor “Wilton Filho”
+// disciplina = disciplina + " Prof. Wilton Filho"
+
+// // Declarar uma constante e atribuir a ela o valor “IFTM”
+// const IES = "IFTM";
+
+// // Declarar uma variável qualquer sem dado
+// var x;
+
+// // Declarar uma variável e atribuir a ela o valor “10 anos”
+// var idade = "10 anos";
+
+// // Declarar uma variável para armazenar o número inteiro da variável anterior, usando parseInt
+// var idade2 = parseInt(idade);
+
+// // Exibir no console o conteúdo de todas as variáveis
+// console.log(disciplina, IES, x, idade, idade2);
+// // console.log(IES);
+
+// // Declarar uma variável com o dado “Valor 20”
+// var y = "valor 20";
+
+// // Declarar uma variável para armazenar o número inteiro da variável anterior usando parseInt
+// var z = parseInt(y);
+
+// // Exibir no console o conteúdo de todas as variáveis
+// console.log(disciplina, IES, x, idade, idade2, y, z);
+
+// var nro = 7;
+
+// var resultado = nro / 6;
+
+// console.log(resultado.toFixed(2));
+
+// console.clear();
+
+// var i = 10;
+
+// console.log(i);
+
+// i++; // i = i + 1; operador pós-incremento
+
+// //++i;
+
+// console.log(i);
+
+// var idades = [];
+// idades[0] = 10;
+// idades[1] = 20;
+
+// console.log(idades);
+
+// console.clear();
+
+// var letras = ["a", "d", "i", "m", "r","z"];
+
+// //console.log(letras[1]);
+
+// letras[1] = "b";
+
+// //console.log(letras);
+
+// //console.log(`${letras.length} letras`);
+
+// //console.log(letras[0]);
+// //console.log(letras[1]);
+// console.log(letras[letras.length-1]);
+
+// console.clear();
+
+// var vet = [1, 2, 3, 4];
+
+// itemRemovido = vet.pop();
+
+// console.log(vet);
+// console.log(itemRemovido);
+
+// console.clear();
+
+// var vet = [1, 2, 3, 4];
+
+// vet.reverse(); // [4, 3, 2, 1]
+
+// itemRemovido = vet.pop(); // 1
+
+// vet.reverse(); // [2, 3, 4]
+
+// console.log(itemRemovido);
+
+// console.clear();
+
+// var vet = [1, 2, 3, 4];
+
+// itemRemovido = vet.reverse().pop(); // [4, 3, 2, 1]
+
+// vet.reverse(); // [2, 3, 4]
+
+// console.log(itemRemovido);
+
+// console.log(vet)
+
+// console.clear();
+
+// var vet = [1, 2, 3, 4];
+
+// elementosRemovidos = vet.splice(1,2);
+
+// //console.log(elementosRemovidos);
+// console.log(vet);
+
+// console.clear();
+
+// var vet = [1, 2, 3, 4];
+
+// vet.splice(2,0,5,6);
+
+// console.log(vet);
+
+
