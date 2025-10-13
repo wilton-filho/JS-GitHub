a379850c2a57bf551fd4bcc2c748a0c51698e34b
Futuresenaweb <futuresenaweb@gmail.com>
2025-10-13 16:21:13 -0300

    Upload - Etapa 11 - Jogo da memoria

---
+++ projetos/jogoDaMemoria/etapa11/js/cartas.js
@@ -0,0 +1,102 @@
+const _0x5897d8 = _0x5da4;
+(function (_0x33ccda, _0x59f2e6) {
+    const _0x5cceea = _0x5da4, _0xf81430 = _0x33ccda();
+    while (!![]) {
+        try {
+            const _0x2570aa = -parseInt(_0x5cceea(0x168)) / (0x21e0 + 0x1dbb + -0x3f9a) * (-parseInt(_0x5cceea(0x162)) / (0x1483 + 0x2234 + -0x36b5)) + parseInt(_0x5cceea(0x15f)) / (-0x1b9a + -0xb5c * 0x1 + 0x26f9) * (-parseInt(_0x5cceea(0x17a)) / (0xd59 + 0x30b + 0x20c * -0x8)) + -parseInt(_0x5cceea(0x166)) / (0x178d + -0x1 * 0x1421 + -0x367) * (-parseInt(_0x5cceea(0x165)) / (0x1e * -0x13d + 0x13e7 + -0x1145 * -0x1)) + parseInt(_0x5cceea(0x161)) / (0x125 * -0xf + 0x1699 + 0x567 * -0x1) * (parseInt(_0x5cceea(0x16c)) / (-0xa1c * -0x2 + 0x6 * -0x62d + 0x10de)) + parseInt(_0x5cceea(0x167)) / (0x1cfe + 0xb27 + -0x281c) + parseInt(_0x5cceea(0x174)) / (0x1f3 * -0xb + 0x148d + 0xee) * (-parseInt(_0x5cceea(0x182)) / (-0x94b + 0x3ac * 0x5 + -0x906)) + -parseInt(_0x5cceea(0x171)) / (-0x20d9 + -0x2a5 * 0x3 + 0x28d4 * 0x1);
+            if (_0x2570aa === _0x59f2e6)
+                break;
+            else
+                _0xf81430['push'](_0xf81430['shift']());
+        } catch (_0x8c0b08) {
+            _0xf81430['push'](_0xf81430['shift']());
+        }
+    }
+}(_0x336c, 0x11fa36 + -0x2 * 0x9cdd6 + -0x22fb * -0x68));
+function _0x5da4(_0x303464, _0x2061a5) {
+    const _0x101eba = _0x336c();
+    return _0x5da4 = function (_0x4cc46e, _0x578786) {
+        _0x4cc46e = _0x4cc46e - (0x1dc4 * 0x1 + -0x23e + 0xb5 * -0x25);
+        let _0x2b4e72 = _0x101eba[_0x4cc46e];
+        return _0x2b4e72;
+    }, _0x5da4(_0x303464, _0x2061a5);
+}
+function _0x336c() {
+    const _0x1af549 = [
+        'tres_paus',
+        '10728828sjtNCN',
+        'dama_ouro',
+        'rei_espada',
+        '7855090QAVPrp',
+        'dois_paus',
+        'valete_pau',
+        'sete_ouro',
+        'valete_cor',
+        'valete_esp',
+        '8JOrvns',
+        'valete_our',
+        'as_paus',
+        'dois_espad',
+        'quatro_pau',
+        'sete_corac',
+        'dana_espad',
+        'tres_corac',
+        '22VafIIq',
+        'as_coracao',
+        'rei_coraca',
+        'dois_ouro',
+        'dama_paus',
+        'rei_paus',
+        '227319YfGaKB',
+        'acao',
+        '433594caDRBl',
+        '2306IRurGK',
+        'rei_ouro',
+        'as_ouro',
+        '12TFDySS',
+        '190565HKZXGS',
+        '10635984WkRIAc',
+        '1195Fbbawk',
+        'dois_corac',
+        'tres__ouro',
+        'as_espada',
+        '104LlRPIt',
+        'dama_corac',
+        'ada',
+        'tres_espad'
+    ];
+    _0x336c = function () {
+        return _0x1af549;
+    };
+    return _0x336c();
+}
+const cartas = [
+    { 'carta': _0x5897d8(0x17c) },
+    { 'carta': _0x5897d8(0x175) },
+    { 'carta': _0x5897d8(0x170) },
+    { 'carta': _0x5897d8(0x17e) + 's' },
+    { 'carta': _0x5897d8(0x176) + 's' },
+    { 'carta': _0x5897d8(0x15d) },
+    { 'carta': _0x5897d8(0x15e) },
+    { 'carta': _0x5897d8(0x164) },
+    { 'carta': _0x5897d8(0x185) },
+    { 'carta': _0x5897d8(0x16a) },
+    { 'carta': _0x5897d8(0x177) },
+    { 'carta': _0x5897d8(0x17b) + 'o' },
+    { 'carta': _0x5897d8(0x172) },
+    { 'carta': _0x5897d8(0x163) },
+    { 'carta': _0x5897d8(0x183) },
+    { 'carta': _0x5897d8(0x169) + 'ao' },
+    { 'carta': _0x5897d8(0x181) + 'ao' },
+    { 'carta': _0x5897d8(0x17f) + 'ao' },
+    { 'carta': _0x5897d8(0x178) + _0x5897d8(0x160) },
+    { 'carta': _0x5897d8(0x16d) + 'ao' },
+    { 'carta': _0x5897d8(0x184) + 'o' },
+    { 'carta': _0x5897d8(0x16b) },
+    { 'carta': _0x5897d8(0x17d) + 'a' },
+    { 'carta': _0x5897d8(0x16f) + 'a' },
+    { 'carta': _0x5897d8(0x179) + _0x5897d8(0x16e) },
+    { 'carta': _0x5897d8(0x180) + 'a' },
+    { 'carta': _0x5897d8(0x173) }
+];
+export default cartas;
